fc22e01a5ad0cadf1e4a4361982e98b36e0c992c
sooto selim <adewaleselim6@gmail.com>
2022-11-29 22:41:41 -1200

    finish everthing up

--- src/Client/Interface/Navbar/Navbar.js
+++ src/Client/Interface/Navbar/Navbar.js
@@ -95,7 +95,7 @@ function Navbar() {
 
                     {/* sending setsubscribe to prof so i can toggle the profile. I also send setnavstate so i can turn hamburger to 'hidden' when profile is clicked */}
                     {/* i also want to show the prof button only when the user is online */}
-                    {/* {currentUser && <Prof subscribe={subscribe} setsubscribe={setsubscribe} setnavState={setnavState} />} */}
+                    {currentUser && <Prof subscribe={subscribe} setsubscribe={setsubscribe} setnavState={setnavState} />}
 
                     <div className="block lg:hidden">
                         {/* <button  id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"> */}
